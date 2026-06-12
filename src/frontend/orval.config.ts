522d0e4a7f28498614274681a7c7f80c4d5e949f
Tom Burger <tburger@glaass.net>
2026-06-12 20:25:37 +0200

    fixing deployment script

--- src/frontend/orval.config.ts
+++ src/frontend/orval.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from 'orval';
 const orvalConfig = defineConfig({
   api: {
     input: {
-      target: '../backend/storage/app/scribe/openapi.yaml',
+      target: '../storage/app/scribe/openapi.yaml',
     },
     output: {
-      target: './src/services/api.ts',
+      target: './services/api.ts',
       client: 'axios',
       mode: 'tags-split',
       prettier: true,
